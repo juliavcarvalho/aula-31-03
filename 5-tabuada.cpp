c6c759e7ae464a1f08c1c4b3501cd72ff6d0d798
juliavcarvalho <jvmouracarvalho02@gmail.com>
2025-04-14 17:56:11 -0300

    5-tabuada.cpp

---
+++ 5-tabuada.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+int main() {
+    
+    cout << "Olá! Para começar, digite um número: " << endl;
+    int num1 = 0;
+    cin >> num1;
+    
+    cout << "Aqui estão os resultados da tabuada: " << " \n " 
+    << endl;
+    
+    cout << num1 << " X 0 = " << num1 * 0 << endl;
+    cout << num1 << " X 1 = " << num1 * 1 << endl;
+    cout << num1 << " X 2 = " << num1 * 2 << endl;
+    cout << num1 << " X 3 = " << num1 * 3 << endl;
+    cout << num1 << " X 4 = " << num1 * 4 << endl;
+    cout << num1 << " X 5 = " << num1 * 5 << endl;
+    cout << num1 << " X 6 = " << num1 * 6 << endl;
+    cout << num1 << " X 7 = " << num1 * 7 << endl;
+    cout << num1 << " X 8 = " << num1 * 8 << endl;
+    cout << num1 << " X 9 = " << num1 * 9 << endl;
+    cout << num1 << " X 10 = " << num1 * 10 << endl;
+    
+    return 0;
+}
